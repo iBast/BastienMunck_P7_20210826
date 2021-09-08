057cd92ddddb5a8c3e39f0e6c513c70e80240c26
Bastien <bastien.munck@me.com>
2021-09-08 08:42:40 +0200

    Refactor creat abstract representation

--- src/Representation/Products.php
+++ src/Representation/Products.php
@@ -7,14 +7,13 @@ use Pagerfanta\Pagerfanta;
 use JMS\Serializer\Annotation\Type;
 
 
-class Products
+class Products extends AbstractRepresentation
 {
 
     /**
      * @Type("ArrayIterator<App\Entity\Product>")
      */
     public $data;
-    public $meta;
 
     public function __construct(Pagerfanta $data)
     {
@@ -24,19 +23,5 @@ class Products
         $this->addMeta('current_items', iterator_count($data->getCurrentPageResults()));
         $this->addMeta('total_items', $data->getNbResults());
         $this->addMeta('page', $data->getCurrentPage());
-    }
-
-    public function addMeta($name, $value)
-    {
-        if (isset($this->meta[$name])) {
-            throw new \LogicException(sprintf('This meta already exists. You are trying to override this meta, use the setMeta method instead for the %s meta.', $name));
-        }
-
-        $this->setMeta($name, $value);
-    }
-
-    public function setMeta($name, $value)
-    {
-        $this->meta[$name] = $value;
     }
 }
